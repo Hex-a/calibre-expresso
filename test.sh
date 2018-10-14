0bcf00f6063011d7b09cc129f68751ad1657d439
Hex-a <mc.pires@campus.fct.unl.pt>
2018-10-14 14:34:54 +0100

    Add arguments to test.sh to control the number of feeds and articles are fetched.

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-ebook-convert expresso.recipe .epub --test -vv --debug-pipeline debug --username $1 --password $2
+ebook-convert expresso.recipe .epub --test $3 $4 -vv --debug-pipeline debug --username $1 --password $2
